2fe5d898337ed15ae42b35d11aff75d2d4ae9a8c
PaoloLaMuro <paololamuro@gmail.com>
2025-04-12 14:32:34 +0200

    Primo Commit
    creazione progetto + application properties

---
+++ src/main/java/it/uniroma3/siw/LudotecaVirtualeApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudotecaVirtualeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudotecaVirtualeApplication.class, args);
+	}
+
+}
