8a75225f8ca7433db0d666018622cb5c5f8c0a3e
PaoloLaMuro <paololamuro@gmail.com>
2025-04-12 14:54:18 +0200

    secondo commit application properties aggiunto iniziata creazione classe Customer

---
+++ src/main/java/it/uniroma3/siw/Ludoteca/Customer.java
@@ -0,0 +1,19 @@
+package it.uniroma3.siw.Ludoteca;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String phoneNumber;     
+
+}
